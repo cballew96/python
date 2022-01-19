d8590563f1344de4a9339307da2496fdf63e5936
cballew96 <38291954+cballew96@users.noreply.github.com>
2022-01-19 17:16:30 -0600

    Update mysqldbconn.py

--- mysqldbconn.py
+++ mysqldbconn.py
@@ -17,6 +17,21 @@ try:
         cursor.execute("select database();")
         record = cursor.fetchone()
         print("You're connected to the database!")
+        cursor.execute('DROP TABLE IF EXISTS players')
 
+        print("Creating Table...")
+        cursor.execute("CREATE TABLE players(name CHAR(255),team CHAR(4),g INT,pa INT,hr INT,"
+                       "r INT,rbi INT,sb INT,bb_percentage DOUBLE,k_percentage DOUBLE,iso DOUBLE,"
+                       "babip DOUBLE,avg DOUBLE,obp DOUBLE,slg DOUBLE,woba DOUBLE,xwoba DOUBLE,"
+                       "wrc_plus INT,bsr DOUBLE,off DOUBLE,def DOUBLE,war DOUBLE,playerid INT NOT NULL,"
+                       "PRIMARY KEY (playerid))")
+        print("Table is created...")
+        # filling in the table created with data from df -- 2021_batting_stats.csv
+        for i,row in df.iterrows():
+            sql = "INSERT INTO baseball.players VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            cursor.execute(sql, tuple(row))
+            print("Record inserted")
+
+            conn.commit()
 except Error as e:
             print("Error while connecting to MySQL", e)
